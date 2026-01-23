0e59f28f020f20ac75d78c2acc9ec9b22e286db8
Valentino <valentinoarhuata18@gmail.com>
2026-01-23 18:49:13 -0500

    base de todo

---
+++ src/pokemon/dto/update-pokemon.dto.ts
@@ -0,0 +1,8 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreatePokemonDto } from './create-pokemon.dto';
+
+export class UpdatePokemonDto extends PartialType(CreatePokemonDto) {
+
+
+    
+}
